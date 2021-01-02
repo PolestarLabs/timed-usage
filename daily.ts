6c8e3bb0017eb945fddf71798a8666696a17d2a2
Bsian <chharry321@gmail.com>
2021-01-02 02:16:18 +0000

    Fixes and changes

--- daily.ts
+++ daily.ts
@@ -1,47 +1,71 @@
-// @ts-nocheck
 const DAY = 22 * 60 * 60e3;
 const EXPIRE = DAY * 2.1;
 
-import { BaseData, Member, Message } from "eris";
+import { BaseData, Member, Message, User } from "eris";
 import { TranslationOptions } from "i18next";
 import { TimedUsage, Req, init as initTimedUsage } from ".";
 import * as Economy from "./types/Economy"
 import { DAILY } from "./utils/Premium";
+// @ts-ignore
 import Moment from "moment";
 import { EventEmitter } from "events";
+import { info } from "console";
+
+interface DailyValues {
+  RBN: number;
+  JDE: number;
+  SPH: number;
+
+  PSM: number;
+  comToken: number;
+  cosmo_fragment: number;
+
+  boosterpack: number;
+  EXP: number;
+
+  stickers: number;
+  evToken: number;
+
+  lootbox_C: number;
+  lootbox_U: number;
+  lootbox_R: number;
+  lootbox_SR: number;
+  lootbox_UR: number;
+}
 
 export class Daily extends EventEmitter {
-  timedUsage: TimedUsage; user: User | BaseData; dailyPLXMember: Member | null; myDaily = {
-    RBN: 0,
-    JDE: 0,
-    SPH: 0,
-
-    PSM: 0,
-    comToken: 0,
-    cosmo_fragment: 0,
-
-    boosterpack: 0,
-    EXP: Math.max(~~(streak / 2), 10),
-
-    stickers: 0,
-    evToken: 0,
-
-    lootbox_C: 0,
-    lootbox_U: 0,
-    lootbox_R: 0,
-    lootbox_SR: 0,
-    lootbox_UR: 0,
-  }
-  constructor(timedUsage: TimedUsage, user: User | BaseData, dailyPLXMember: Member | null) {
+  timedUsage: TimedUsage; user: User | BaseData; dailyPLXMember: Member | null; softStreak!: number; myDaily: DailyValues; userData: any;
+  constructor(timedUsage: TimedUsage, user: User | BaseData, dailyPLXMember: Member | null, userData: any) {
     super();
-    this.timedUsage = timedUsage; this.user = user; this.dailyPLXMember = dailyPLXMember;
+    this.timedUsage = timedUsage; this.user = user; this.dailyPLXMember = dailyPLXMember; this.userData = userData;
+    this.myDaily = {
+      RBN: 0,
+      JDE: 0,
+      SPH: 0,
+  
+      PSM: 0,
+      comToken: 0,
+      cosmo_fragment: 0,
+  
+      boosterpack: 0,
+      EXP: Math.max(~~(this.timedUsage.userDaily.streak / 2), 10),
+  
+      stickers: 0,
+      evToken: 0,
+  
+      lootbox_C: 0,
+      lootbox_U: 0,
+      lootbox_R: 0,
+      lootbox_SR: 0,
+      lootbox_UR: 0,
+    }
   }
 
   awardPrizes(ECO: typeof Economy, actions: Promise<any>[]) {
     const currencies: ["RBN", "JDE", "SPH", "PSM"] = ["RBN", "JDE", "SPH", "PSM"];
     return Promise.all([...actions,
-      ECO.receive(this.userData.id, currencies.map((curr) => this.myDaily[curr]), "Daily Rewards", currencies),
-      DB.users.set(this.userData.id, {
+      ECO.receive(this.user.id, currencies.map((curr) => this.myDaily[curr]), "Daily Rewards", currencies),
+      DB.users.set(this.user.id, {
         $inc: {
           "modules.exp": this.myDaily.EXP || 0,
           eventTokens: this.myDaily.evToken || 0,
@@ -60,17 +84,17 @@ export class Daily extends EventEmitter {
       // let streak = Number(args[0]||1)
       const { streak } = this.timedUsage.userDaily;
   
-      const softStreak = streak % 10 || 10;
+      this.softStreak = streak % 10 || 10;
       const is = (x: number) => !(streak % x);
       const isRoadTo = (n: number) => (streak % n) > n - 10;
   
-      this.emit("softStreak", softStreak);
+      this.emit("softStreak", this.softStreak);
   
       //
       if (isRoadTo(50) && !is(50)) this.emit("isRoadTo50-isNot50");
       if (isRoadTo(100) && !is(100)) this.emit("isRoadTo100-isNot100");
   
-      switch (softStreak) {
+      switch (this.softStreak) {
         case 1: case 2: this.myDaily.RBN += 150;
         case 3: this.myDaily.JDE += 1000;
         case 4: case 8: this.myDaily.cosmo_fragment += 25;
@@ -115,7 +139,6 @@ export class Daily extends EventEmitter {
         this.emit("guildBooster");
       }
   
-      // (apart from RBN += donoBoost)
       if (this.userData.donator) {
         const donoBoost = DAILY[this.userData.donator as keyof typeof DAILY] || 0;
         this.emit("userDonator", donoBoost);
@@ -130,8 +153,8 @@ export class Daily extends EventEmitter {
       return (message as Message).channel.send(_emoji!("nope") + $t("responses.daily.dailyNope", P));
     }
 
-    initTimedUsage(
-      input,
+    initTimedUsage( // @ts-ignore
+      input.author || input.user,
       "daily",
       { day: DAY, expiration: EXPIRE, streak: true },
       success, reject, info,
